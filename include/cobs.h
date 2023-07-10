29183650a5a576908d7e294dda26eedb177ad41d
Michael Zimmermann <michael.zimmermann@grandcentrix.net>
2023-07-10 06:22:17 +0200

    cobs_decode*: support zero-length decoded data
    Previously those functions returned 0 on error. The issue with that is
    that 0 can be a valid length for the decoded data.
    
    Example (with trailing 0-byte):
    0x01 0x00

--- include/cobs.h
+++ include/cobs.h
@@ -24,22 +24,24 @@ size_t cobs_encode(const uint8_t *restrict input, size_t length, uint8_t *restri
 
 /**
  * Unstuffs "length" bytes of data at the location pointed to by
- * "input", writing the output * to the location pointed to by
- * "output". Returns the number of bytes written to "output" if
- * "input" was successfully unstuffed, and 0 if there was an
- * error unstuffing "input".
+ * "input", writing the output to the location pointed to by
+ * "output". On success, returns 0 and writes the the number of bytes
+ * that were written to "output" to "decoded_size". On failure, it
+ * returns a negative errno code.
  *
  * Remove the "restrict" qualifiers if compiling with a
  * pre-C99 C dialect.
  */
-size_t cobs_decode(const uint8_t *restrict input, size_t length, uint8_t *restrict output);
+int cobs_decode(const uint8_t *restrict input, size_t length, uint8_t *restrict output,
+		size_t *decoded_size);
 
 /**
- * Unstuffs "length" bytes of data at the location pointed to by
+ * Unstuffs "max_length" bytes of data at the location pointed to by
  * "data", in-place, over-writing the original.
- * Returns the number of bytes of the decoded data if it was
- * successfully unstuffed, and 0 if there was an error unstuffing.
+ * On success, returns 0 and writes the the number of bytes
+ * that were written to "data" to "decoded_size". On failure, it
+ * returns a negative errno code.
  */
-size_t cobs_decode_inplace(uint8_t *restrict data, size_t max_length);
+int cobs_decode_inplace(uint8_t *restrict data, size_t max_length, size_t *decoded_size);
 
 #endif
